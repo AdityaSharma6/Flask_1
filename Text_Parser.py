b9605dc61dc45c222269a7397457fc53ffe9b6a6
Aditya Sharma <22263756+AdityaSharma6@users.noreply.github.com>
2019-08-04 21:15:39 -0400

    Text Parser Complete

---
+++ Text_Parser.py
@@ -0,0 +1,28 @@
+
+class Text_Text_Extraction():
+    def __init__(self, file_name):
+        self.file_name = file_name
+    
+    def text_extraction(self):
+        with open(self.file_name) as data:
+            contents = data.readlines()
+            print(contents)
+
+            counter = 0
+            while counter in range(len(contents)):
+                if (contents[counter] == "\n"):
+                    contents.pop(counter)
+                    continue
+                contents[counter] = contents[counter][:-1]
+                counter += 1
+                
+            
+        text = " ".join(contents)
+        print(text)
+        return text
+
+    def execute(self):
+        self.text_extraction()
+
+parser = Text_Text_Extraction("file.txt")
+parser.execute()
